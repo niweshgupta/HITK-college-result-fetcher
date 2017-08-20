1ba5b02b7307d447ab4a648eb635ff14250acebd
Niwesh Gupta <niweshgupta@gmail.com>
2017-08-20 16:35:14 +0530

    Range Query

--- result_ranker.py
+++ result_ranker.py
@@ -1,25 +1,35 @@
 import os,requests
 
-def bekar(haystack, needle):
+def get_data(haystack, needle):
 	a = haystack.split(needle)
 	b = a[1].split("</span>")
 	return b[0]
 
 f1 = open("result.txt", 'w+')
 final = []
-for x in range(12616001001, 12616001189) :
-	r = requests.post("http://61.12.70.61:8084/heresult17.aspx", data={'roll': x, 'sem': 2})
+print "Enter the roll number range (eg : 16616001001 12616001189) : "
+start, end = raw_input().split()
+print "Enter the sem number (eg : 2) : "
+sem = int(input())
+print "Enter your choice : "
+print "1. Get Results sorted alphabetically"
+print "2. Get Results sorted rankwise"
+choice = int(input())
+for x in range(int(start), int(end)) :
+	r = requests.post("http://61.12.70.61:8084/heresult17.aspx", data={'roll': x, 'sem': sem})
 	string = r.text
 	if string == "No such student exists in this database" :
 		continue
-	name = bekar(string, "<span id=\"lblname\">Name  ")
-	roll = bekar(string, "<span id=\"lblroll\">Roll No.  ")
-	SGPA = bekar(string, "<span id=\"lblbottom2\">SGPA       EVEN(2nd.) SEMESTER: ")
+	name = get_data(string, "<span id=\"lblname\">Name  ")
+	roll = get_data(string, "<span id=\"lblroll\">Roll No.  ")
+	SGPA = get_data(string, "<span id=\"lblbottom2\">SGPA       EVEN(2nd.) SEMESTER: ")
 	name = name.encode('ascii', 'ignore').decode('ascii')
-	tup = (SGPA, name, roll)
-	final.append(tup)
+	if choice == 1 :
+		f1.write(" ".join([roll, name, SGPA, '\n']))
+	else :
+		tup = (SGPA, name, roll)
+		final.append(tup)
 	print x
-	# f1.write(" ".join([roll, name, SGPA, '\n']))
 final.sort(reverse = True)
 rank = 1
 for s, name, r in final:
